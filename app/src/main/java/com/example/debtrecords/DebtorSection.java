d1fa2d387074a46a0d3e0cf156e0dd7340748a80
latifa176 <55226346+latifa176@users.noreply.github.com>
2023-04-13 06:52:58 +0300

    String values for the enum DebtorSection

--- app/src/main/java/com/example/debtrecords/DebtorSection.java
+++ app/src/main/java/com/example/debtrecords/DebtorSection.java
@@ -1,9 +1,19 @@
 package com.example.debtrecords;
 
 public enum DebtorSection {
-    No_Section,
-    First,
-    Second,
-    Third,
-    Other
+    No_Section ("بدون قسم"),
+    First ("أولى"),
+    Second ("ثاني"),
+    Third ("ثالث"),
+    Other ("أخرى");
+    private String value;
+
+    private DebtorSection(String value)
+    {
+        this.value = value;
+    }
+    @Override public String toString()
+    {
+        return value;
+    }
 }
